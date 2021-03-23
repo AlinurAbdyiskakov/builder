56f92ba58d7a7df4a1eea4831575295d7f979a88
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-03-23 13:27:56 +0600

    the chnage in the file Animalpriwet add new snachanei

--- src/components/Animal/AnimalPreview/AnimalPreview.js
+++ src/components/Animal/AnimalPreview/AnimalPreview.js
@@ -5,11 +5,21 @@ const AnimalPreview = () => {
   return (
     <div className={classes.AnimalPreview}>
      <div  className={ classes.color}>
-         <AnimalAll type="regf"/>
        
+         <AnimalAll type="boat"/>
+         <AnimalAll type="boat"/>
+         <AnimalAll type="boat"/>
+         <AnimalAll type="boat"/>
+         <AnimalAll type="boat"/>
+         <AnimalAll type="boat"/>
+         <AnimalAll type="boat"/>
+         <AnimalAll type="ship"/>
+         <AnimalAll type="ship"/><AnimalAll type="ship"/><AnimalAll type="ship"/><AnimalAll type="ship"/><AnimalAll type="ship"/>
+     
      </div>
     </div>
   );
+  
 }
  
 export default AnimalPreview;
