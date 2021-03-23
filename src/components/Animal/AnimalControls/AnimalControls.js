2298efa1a9faed8074bfc8a6f6079d365909dd77
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-03-23 12:58:35 +0600

    AnimalControls file add new text and atrebut

--- src/components/Animal/AnimalControls/AnimalControls.js
+++ src/components/Animal/AnimalControls/AnimalControls.js
@@ -3,8 +3,9 @@ import classes from "./AnimalControls.module.css";
 const AnimalControls = () => {
   return (
     <div className={classes.AnimalControls}>
-   AnimalControls
-    </div>
+ <h1>warship + 1 - </h1>  
+ <h1> cargo ship + 1 - </h1>  
+   </div>
   );
 }
  
