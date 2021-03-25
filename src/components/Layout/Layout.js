6bcb2e72439794a69a87c2a177e9a7869022f39d
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-03-25 14:03:24 +0600

    layout the chnage

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,33 @@
-import classes from "./Layout.module.css";
 import Toolbar from "./Toolbar/Toolbar";
 import Drawer from "./Drawer/Drawer";
-import { useState } from "react";
+import {
+  useState
+} from "react";
+import classes from "./Layout.module.css";
 
-const Layout = ({ children }) => {
+const Layout = ({
+  children
+}) => {
   const [drawerOpen, setDrawerOpen] = useState(false);
 
-  return (
-    <div className={classes.Layout}>
-      <Toolbar openDrawer={() => setDrawerOpen(true)} />
-      <Drawer open={drawerOpen} closeDrawer={() => setDrawerOpen(false)} />
-      {children}
-    </div>
+  return ( <div className = {
+      classes.Layout
+    } >
+    <
+    Toolbar openDrawer = {
+      () => setDrawerOpen(true)
+    }
+    /> <
+    Drawer open = {
+      drawerOpen
+    }
+    closeDrawer = {
+      () => setDrawerOpen(false)
+    }
+    /> {
+      children
+    } </div>
   );
 }
- 
+
 export default Layout;
