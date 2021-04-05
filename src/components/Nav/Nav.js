042053d4da296e100133504f1b2ea969c6eeb361
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-04-05 19:14:34 +0600

    the chnage in the file  Nav.js add new znacenie

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,8 +4,8 @@ import classes from "./Nav.module.css";
 const Nav = () => {
   return (
     <div className={classes.Nav}><ul>
-      <li><a href="./Animal.js">Pet shop builder</a></li>
-      <li><a href="./Navorder.js">Authentacte</a></li>
+      <li><a href="./Animal/Animal.js">Pet shop builder</a></li>
+      <li><a href="./Navorder/Navorder.js">Authentacte</a></li>
     </ul></div>
   );
 }
