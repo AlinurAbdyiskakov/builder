6f8957e67451bc8ab8fd826818221f6868282e88
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-04-08 15:42:59 +0600

    Toolbar add media

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,12 @@
 
 import classes from "./Nav.module.css";
-import Navbuilder from "./Navbuilder/Navbuilder"
+import NavItem from "./NavItem/NavItem"
 const Nav = () => {
   return (
-    <div className={classes.Nav}><ul>
-      <Navbuilder url="/" active>Pet shop builder</Navbuilder>
-      <Navbuilder url="/">Authentacte</Navbuilder>
-    </ul></div>
+    <ul className={classes.Nav}>
+      <NavItem url="/" active>Pet shop builder</NavItem>
+      <NavItem url="/">Authentacte</NavItem>
+    </ul>
   );
 }
 
