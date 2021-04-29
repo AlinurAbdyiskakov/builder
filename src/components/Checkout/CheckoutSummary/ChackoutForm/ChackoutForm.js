a73a290f25b702b5fd92a861d7081f3f8d71aa85
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-04-29 15:06:10 +0600

    the change all the tedxt  add new teext

--- src/components/Checkout/CheckoutSummary/ChackoutForm/ChackoutForm.js
+++ src/components/Checkout/CheckoutSummary/ChackoutForm/ChackoutForm.js
@@ -8,7 +8,7 @@ const ChacoutForm = ({submitCallback, cancelCallback }) => {
         <input type="tel" id="phone" name="phone" placeholder="0552955915" required pattern="0[0-9]{9}"></input></div>
         <div><label for="address">Address</label>
         <input type="text" id="address" name="address"></input></div>
-        <Button>Checkout</Button>
+        <Button >Checkout</Button>
         <Button onClick={cancelCallback}>Cancel</Button>
     </form>
 
