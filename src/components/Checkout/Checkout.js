685a37176d3023f152652bfd4915c02d4e3dc88c
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-04-29 15:28:31 +0600

    add firbase k ChackoutForm

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import CheckoutSummary from "./CheckoutSummary/CheckoutSummary"
 
 const Checkout = ({ history }) => {
@@ -13,7 +14,19 @@ const Checkout = ({ history }) => {
       name: data.get('name'),
       phone: data.get('phone'),
       address: data.get('address'),
+      ingredients:{
+        Dog: 80,
+        Pig: 70,
+        Mouse:60,
+        Cat:20,
+        Reccoon: 67,
+        Parrot:45,
+      }
     }
+    axios.post('https://builder-e02c1-default-rtdb.firebaseio.com/orders.json',order)
+    .then(response =>{
+      history.replace('/');
+    })
 
     console.log(order)
   }
