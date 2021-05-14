3b62f43e7c84b4dbb5e61223f6e846d3bb0da743
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-05-14 09:26:04 +0600

    the change svg icons na new svg icons

--- src/components/Animal/AnimalAll/AnimalAll.js
+++ src/components/Animal/AnimalAll/AnimalAll.js
@@ -1,19 +1,19 @@
 import classes from "./AnimalAll.module.css";
 import dogBackground from "../../../imges/about.svg";
-import cat from "../../../imges/cat.svg";
-import chameleon from "../../../imges/pig.svg";
-import reccoon from "../../../imges/raccoon.svg";
-import turtle from"../../../imges/mouse.svg";
+import cat from "../../../imges/help.svg";
+import chameleon from "../../../imges/say.svg";
+import reccoon from "../../../imges/look3.svg";
+import turtle from"../../../imges/good.svg";
 import Hamster from "../../../imges/spell1.svg";
 
 const AnimalAll = ({ type ,fixed }) => {
   const types = {
-  Dog: { backgroundImage: `url(${dogBackground})`, width: "50px", height: "50px"},
-  Cat: { backgroundImage: `url(${cat})`, width: "50px", height: "50px" },
+  Dog: { backgroundImage: `url(${dogBackground})`, width: "60px", height: "60px"},
+  Cat: { backgroundImage: `url(${cat})`, width: "60px", height: "60px" },
   Chameleon: { backgroundImage: `url(${chameleon})`, width: "60px", height: "60px",},
-  Reccoon: { backgroundImage: `url(${reccoon})`, width: "50px", height: "50px"  },
-  Turtle:{ backgroundImage: `url(${turtle})`, width: "60px", height: "60px"  },
-  Hamster:{ backgroundImage: `url(${Hamster})`, width: "70px", height: "70px"  },
+  Reccoon: { backgroundImage: `url(${reccoon})`, width: "60px", height: "60px"  },
+  Turtle:{ backgroundImage: `url(${turtle})`, width: "90px", height: "90px"  },
+  Hamster:{ backgroundImage: `url(${Hamster})`, width: "60px", height: "60px"  },
   }
  
   return (
