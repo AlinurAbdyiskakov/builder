4c1d17d6a9dc8ff5995ca943e11e0bcdfb00e707
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-05-25 15:53:45 +0600

    in the actions auth the change success new add dispatch

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -32,7 +32,8 @@ export const restore = () => {
     const localId = localStorage.getItem('localId');
 
     if (idToken && localId) {
-      success(dispatch, { idToken, localId });
+      dispatch(success({idToken,localId}));
+  
     }
     else {
       dispatch(logout());
