ababbedc33073fea02eaef28d0db1de2859cf927
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-05-26 15:04:08 +0600

    dlect  div s classes bold in the Chacout.js

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -4,8 +4,11 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router";
 import { auth, start } from "../../store/actions/auth";
-
+// import showPwdImg from '../../imges/show-password.svg';
+// import hidePwdImg from "../../imges/hide-password.svg";
 const Auth = () => {
+  // const [pwd, setPwd] = useState('');
+  // const [isRevealPwd, setIsRevealPwd] = useState(false);
   const dispatch = useDispatch();
   const { token, error, loading } = useSelector(state => state.auth);
   const [method, setMethod] = useState("signin");
@@ -37,6 +40,22 @@ const Auth = () => {
       <form onSubmit={submitCallback} className={classes.input}>
         <input type="email" name="email" placeholder="Email" required />
         <input type="password" name="password" placeholder="Password" required />
+        {/* <div className="pwd-container">
+        <input
+          name="pwd"
+          placeholder="Enter Password"
+          type={isRevealPwd ? "text" : "password"}
+          value={pwd}
+          onChange={e => setPwd(e.target.value)}
+          required
+        />
+        <img 
+          title={isRevealPwd ? "Hide password" : "Show password"}
+          src={isRevealPwd ? hidePwdImg : showPwdImg}
+          onClick={() => setIsRevealPwd(prevState => !prevState)}
+       alt={"heloo"} />
+      </div> */}
+
         <div className={classes.color}>
         <button onClick={() => setMethod("signin")} disabled={loading}>Зойти</button>
         <button onClick={() => setMethod("signup")} disabled={loading}>Регистрация</button>
